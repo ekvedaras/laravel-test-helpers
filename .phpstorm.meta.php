fde2dc83ec4c079aa5d82672003de1bfef008c06
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2018-08-11 08:10:16 +0000

    add: .phpstorm.meta.php for BuildsMocks::mock() method

---
+++ .phpstorm.meta.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PHPSTORM_META {
+    override(\Ekvedaras\LaravelTestHelpers\Traits\Helpers\BuildsMocks::mock(0), map([
+        '' => '@|\Ekvedaras\LaravelTestHelpers\Helpers\TestHelpersMock',
+    ]));
+}
